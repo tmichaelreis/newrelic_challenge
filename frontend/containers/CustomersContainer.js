a6d77967452771b033804e0a68d725ce77bcd61d
Tom Reis <tom@ckdtech.co>
2021-06-13 16:43:34 -0600

    Create customer search input component

--- frontend/containers/CustomersContainer.js
+++ frontend/containers/CustomersContainer.js
@@ -1,9 +1,19 @@
-const CustomersContainer = () => (
-  <div>
-    <div>TODO: search</div>
-    <div>TODO: filter</div>
-    <div>TODO: results</div>
-  </div>
-);
+import CustomerSearchInput from "../components/CustomerSearchInput.js";
+
+const CustomersContainer = () => {
+  const handleUserSearch = (value) => {
+    // TODO: update param in URL
+    // TODO: set loading
+    // TODO: handle request to api
+  };
+
+  return (
+    <div>
+      <CustomerSearchInput onChange={handleUserSearch} />
+      <div>TODO: filter</div>
+      <div>TODO: results</div>
+    </div>
+  );
+};
 
 export default CustomersContainer;
