36463c5ddeb3f338ccc971e4cc5050f8b7612823
Tom Reis <tom@ckdtech.co>
2021-06-12 23:09:38 -0600

    Implement Customer full name search using pg_search gem

--- customer_service/app/models/customer.rb
+++ customer_service/app/models/customer.rb
@@ -1,8 +1,16 @@
 class Customer < ApplicationRecord
+  include PgSearch::Model
+
   belongs_to :company
 
   validates :first_name, :last_name, presence: true
 
   # Expose company_name for Customer records
   delegate :name, to: :company, prefix: true
+
+  pg_search_scope :search_by_full_name,
+                  against: [:first_name, :last_name],
+                  using: {
+                    tsearch: { prefix: true }
+                  }
 end
