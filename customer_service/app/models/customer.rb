a58cbb2252fafd736af82dde7bde670309675f61
Tom Reis <tom@ckdtech.co>
2021-06-12 22:35:46 -0600

    Customize serialization of Customers resources

--- customer_service/app/models/customer.rb
+++ customer_service/app/models/customer.rb
@@ -2,4 +2,7 @@ class Customer < ApplicationRecord
   belongs_to :company
 
   validates :first_name, :last_name, presence: true
+
+  # Expose company_name for Customer records
+  delegate :name, to: :company, prefix: true
 end
