9832a8ccf683486cdbc82c3340eeedcf64cdb98d
Tom Reis <tom@ckdtech.co>
2021-06-13 22:48:43 -0600

    Create seed data

--- customer_service/db/seeds.rb
+++ customer_service/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+## Warning: this data is used for integration testing
+new_relic = Company.create({ name: 'New Relic' })
+la_sportiva = Company.create({ name: 'La Sportiva' })
+
+Customer.create({ first_name: 'Tom', last_name: 'Reis', company: new_relic })
+Customer.create({ first_name: 'Adam', last_name: 'Ondra', company: la_sportiva })
+Customer.create({ first_name: 'Brooke', last_name: 'Raboutou', company: la_sportiva })
+###
+
+# Generate random data
+10.times do
+  Company.create({ name: FFaker::Company.name })
+end
+
+100.times do
+  Customer.create({ first_name: FFaker::Name.first_name,
+                    last_name: FFaker::Name.last_name,
+                    company_id: Company.pluck(:id).sample })
+end
