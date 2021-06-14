9751661c7afbb44884783985e3162dd7724adf12
Tom Reis <tom@ckdtech.co>
2021-06-14 10:43:26 -0600

    Search customers by company id instead of name

--- customer_service/app/controllers/customers_controller.rb
+++ customer_service/app/controllers/customers_controller.rb
@@ -9,18 +9,18 @@ class CustomersController < ApplicationController
 
   def customer_results
     search_param = query_params[:search]
-    company_name = query_params[:company_name]
+    company_id = query_params[:company_id]
 
     # Select which queries to chain
     queries = [:all]
     queries << [:search_by_full_name, search_param] if search_param
-    queries << [:with_company_name, company_name] if company_name
+    queries << [:where, { company_id: company_id }] if company_id
 
     # Apply queries to Customer model
     queries.inject(Customer) { |model, method| model.send(*method) }
   end
 
   def query_params
-    params.permit(:search, :company_name)
+    params.permit(:search, :company_id)
   end
 end
